f37c587cea038384e60fa6f80ec8fbb1fcdcc50b
mattgrannary <me@mattgrannary.com>
2016-01-12 22:13:39 -0500

    updated bower and package

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+
 module.exports = {
-  name: 'ember-iscroll'
+  name: 'ember-iscroll',
+
+  treeForVendor: function(tree) {
+    var iscrollPath = path.dirname(require.resolve('iscroll'));
+    var iscrollTree = pickFiles(this.treeGenerator(iscrollPath), {
+      srcDir: '/',
+      destDir: 'iscroll'
+    });
+
+    let trees = iscrollTree;
+    if (tree) {
+      trees = mergeTrees([tree, iscrollTree]);
+    }
+
+    return trees;
+  },
+
+  included: function(app) {
+    this._super.included.apply(this, ...arguments);
+
+    app.import('vendor/iscroll/iscroll.js');
+  },
+
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
